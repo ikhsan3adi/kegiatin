4ac6912db3ee5408eddcf2727064d63a654aa7c5
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2026-04-22 19:40:25 +0700

    Use openapi.yaml from master
    Update index.html

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://github.com/ikhsan3adi/kegiatin/blob/master/.docs/openapi.yaml?raw=true",
+    url: "openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
